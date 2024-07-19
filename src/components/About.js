58bbc4e3cccea1ddfa494d12a4e0ff5a6d017ee0
maylin155 <maylinlattmoe123@gmail.com>
2024-07-20 00:40:07 +0800

    changed link

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,8 @@
-import { Link } from "react-router-dom"
 const About = () => {
   return (
     <div>
         <h4>Version 1.0.0</h4>
-        <Link to="/">Go back</Link>
+        <a href="/">Go back</a>
     </div>
   )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,8 @@
-import { Link } from "react-router-dom"
 const Footer = () => {
   return (
     <footer>
         <p>Copyright &copy; 2024</p>
-        <Link to='/about'>About</Link>
+        <a href='/about'>About</a>
     </footer>
   )
 }
